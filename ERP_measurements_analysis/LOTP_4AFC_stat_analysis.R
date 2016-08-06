7b47082136ac7097c73e2a70917f6db6372fdc7f
Kyle Frankovich <kylefrankovich@gmail.com>
2016-08-05 19:15:53 -0700

    simplified/improved stats script

--- ERP_measurements_analysis/LOTP_4AFC_stat_analysis.R
+++ ERP_measurements_analysis/LOTP_4AFC_stat_analysis.R
@@ -51,304 +51,79 @@ df_mixed_all_resp_SPCN <- read.table('mixed_all_resp_SPCN.txt',
                                     sep="\t", header = TRUE)
 
 
-####### stats:
+# filtered data frames/factorization:
 
-## blocked, correct, N2pc: df_blocked_correct_N2pc;
-
-# summary: no significant N2pc difference between fast/slow
-
-
-View(df_blocked_correct_N2pc)
-
+# correct responses:
 df_blocked_correct_N2pc$bini = factor(df_blocked_correct_N2pc$bini)
-
-class(df_blocked_correct_N2pc$bini)
-
-# occipital stats:
 df_blocked_correct_N2pc_occip = filter(df_blocked_correct_N2pc, chindex == 12)
-
-View(df_blocked_correct_N2pc_occip)
-
-blocked_correct_N2pc_occip_anova = ezANOVA(
-  data = df_blocked_correct_N2pc_occip
-  , dv = .(value)
-  , wid = .(ERPset)
-  , within = .(bini)
-)
-
-print(blocked_correct_N2pc_occip_anova)
-
-blocked_correct_N2pc_occip_anova_data_output = ezStats(
-  data = df_blocked_correct_N2pc_occip
-  , dv = .(value)
-  , wid = .(ERPset)
-  , within = .(bini)
-)
-
-print(blocked_correct_N2pc_occip_anova_data_output)
-
-ezPlot(
-  data = df_blocked_correct_N2pc_occip
-  , dv = .(value)
-  , wid = .(ERPset)
-  , within = .(bini)
-  , x = bini
-)
-
-# PO7/PO8 stats:
 df_blocked_correct_N2pc_PO7_PO8 = filter(df_blocked_correct_N2pc, chindex == 9)
 
-View(df_blocked_correct_N2pc_PO7_PO8)
-
-blocked_correct_N2pc_PO7_PO8_anova = ezANOVA(
-  data = df_blocked_correct_N2pc_PO7_PO8
-  , dv = .(value)
-  , wid = .(ERPset)
-  , within = .(bini)
-)
-
-print(blocked_correct_N2pc_PO7_PO8_anova)
-
-blocked_correct_N2pc_PO7_PO8_anova_data_output = ezStats(
-  data = df_blocked_correct_N2pc_PO7_PO8
-  , dv = .(value)
-  , wid = .(ERPset)
-  , within = .(bini)
-)
-
-print(blocked_correct_N2pc_PO7_PO8_anova_data_output)
-
-ezPlot(
-  data = df_blocked_correct_N2pc_PO7_PO8
-  , dv = .(value)
-  , wid = .(ERPset)
-  , within = .(bini)
-  , x = bini
-)
-
-
-
-## blocked, correct, SPCN: df_blocked_correct_SPCN;
-
-# summary: no significant SPCN difference between fast/slow
-
-View(df_blocked_correct_SPCN)
-
 df_blocked_correct_SPCN$bini = factor(df_blocked_correct_SPCN$bini)
-
-class(df_blocked_correct_SPCN$bini)
-
-# occipital stats:
 df_blocked_correct_SPCN_occip = filter(df_blocked_correct_SPCN, chindex == 12)
-
-View(df_blocked_correct_SPCN_occip)
-
-blocked_correct_SPCN_occip_anova = ezANOVA(
-  data = df_blocked_correct_SPCN_occip
-  , dv = .(value)
-  , wid = .(ERPset)
-  , within = .(bini)
-)
-
-print(blocked_correct_SPCN_occip_anova)
-
-blocked_correct_SPCN_occip_anova_data_output = ezStats(
-  data = df_blocked_correct_SPCN_occip
-  , dv = .(value)
-  , wid = .(ERPset)
-  , within = .(bini)
-)
-
-print(blocked_correct_SPCN_occip_anova_data_output)
-
-ezPlot(
-  data = df_blocked_correct_SPCN_occip
-  , dv = .(value)
-  , wid = .(ERPset)
-  , within = .(bini)
-  , x = bini
-)
-
-
-# PO7/PO8 stats:
 df_blocked_correct_SPCN_PO7_PO8 = filter(df_blocked_correct_SPCN, chindex == 9)
 
-View(df_blocked_correct_SPCN_PO7_PO8)
-
-blocked_correct_SPCN_PO7_PO8_anova = ezANOVA(
-  data = df_blocked_correct_SPCN_PO7_PO8
-  , dv = .(value)
-  , wid = .(ERPset)
-  , within = .(bini)
-)
-
-print(blocked_correct_SPCN_PO7_PO8_anova)
-
-blocked_correct_SPCN_PO7_PO8_anova_data_output = ezStats(
-  data = df_blocked_correct_SPCN_PO7_PO8
-  , dv = .(value)
-  , wid = .(ERPset)
-  , within = .(bini)
-)
-
-print(blocked_correct_SPCN_PO7_PO8_anova_data_output)
-
-ezPlot(
-  data = df_blocked_correct_SPCN_PO7_PO8
-  , dv = .(value)
-  , wid = .(ERPset)
-  , within = .(bini)
-  , x = bini
-)
-
-##################
-
-## mixed, correct, N2pc: df_mixed_correct_N2pc;
-
-View(df_mixed_correct_N2pc)
-
 df_mixed_correct_N2pc$bini = factor(df_mixed_correct_N2pc$bini)
-
-class(df_mixed_correct_N2pc$bini)
-
-# occipital stats:
 df_mixed_correct_N2pc_occip = filter(df_mixed_correct_N2pc, chindex == 12)
-
-View(df_mixed_correct_N2pc_occip)
-
-mixed_correct_N2pc_occip_anova = ezANOVA(
-  data = df_mixed_correct_N2pc_occip
-  , dv = .(value)
-  , wid = .(ERPset)
-  , within = .(bini)
-)
-
-print(mixed_correct_N2pc_occip_anova)
-
-mixed_correct_N2pc_occip_anova_data_output = ezStats(
-  data = df_mixed_correct_N2pc_occip
-  , dv = .(value)
-  , wid = .(ERPset)
-  , within = .(bini)
-)
-
-print(mixed_correct_N2pc_occip_anova_data_output)
-
-ezPlot(
-  data = df_mixed_correct_N2pc_occip
-  , dv = .(value)
-  , wid = .(ERPset)
-  , within = .(bini)
-  , x = bini
-)
-
-
-# PO7/PO8 stats:
 df_mixed_correct_N2pc_PO7_PO8 = filter(df_mixed_correct_N2pc, chindex == 9)
 
-View(df_mixed_correct_N2pc_PO7_PO8)
-
-mixed_correct_N2pc_PO7_PO8_anova = ezANOVA(
-  data = df_mixed_correct_N2pc_PO7_PO8
-  , dv = .(value)
-  , wid = .(ERPset)
-  , within = .(bini)
-)
-
-print(mixed_correct_N2pc_PO7_PO8_anova)
-
-mixed_correct_N2pc_PO7_PO8_anova_data_output = ezStats(
-  data = df_mixed_correct_N2pc_PO7_PO8
-  , dv = .(value)
-  , wid = .(ERPset)
-  , within = .(bini)
-)
-
-print(mixed_correct_N2pc_PO7_PO8_anova_data_output)
-
-ezPlot(
-  data = df_mixed_correct_N2pc_PO7_PO8
-  , dv = .(value)
-  , wid = .(ERPset)
-  , within = .(bini)
-  , x = bini
-)
-
-
-## mixed, correct, SPCN: df_mixed_correct_SPCN;
-
-View(df_mixed_correct_SPCN)
-
 df_mixed_correct_SPCN$bini = factor(df_mixed_correct_SPCN$bini)
-
-class(df_mixed_correct_SPCN$bini)
-
-# occipital stats:
 df_mixed_correct_SPCN_occip = filter(df_mixed_correct_SPCN, chindex == 12)
-
-View(df_mixed_correct_SPCN_occip)
-
-mixed_correct_SPCN_occip_anova = ezANOVA(
-  data = df_mixed_correct_SPCN_occip
-  , dv = .(value)
-  , wid = .(ERPset)
-  , within = .(bini)
-)
-
-print(mixed_correct_SPCN_occip_anova)
-
-mixed_correct_SPCN_occip_anova_data_output = ezStats(
-  data = df_mixed_correct_SPCN_occip
-  , dv = .(value)
-  , wid = .(ERPset)
-  , within = .(bini)
-)
-
-print(mixed_correct_SPCN_occip_anova_data_output)
-
-ezPlot(
-  data = df_mixed_correct_SPCN_occip
-  , dv = .(value)
-  , wid = .(ERPset)
-  , within = .(bini)
-  , x = bini
-)
-
-
-# PO7/PO8 stats:
 df_mixed_correct_SPCN_PO7_PO8 = filter(df_mixed_correct_SPCN, chindex == 9)
 
-View(df_mixed_correct_SPCN_PO7_PO8)
+# all responses
+df_blocked_all_resp_N2pc$bini = factor(df_blocked_all_resp_N2pc$bini)
+df_blocked_all_resp_N2pc_occip = filter(df_blocked_all_resp_N2pc, chindex == 12)
+df_blocked_all_resp_N2pc_PO7_PO8 = filter(df_blocked_all_resp_N2pc, chindex == 9)
 
-mixed_correct_SPCN_PO7_PO8_anova = ezANOVA(
-  data = df_mixed_correct_SPCN_PO7_PO8
+df_blocked_all_resp_SPCN$bini = factor(df_blocked_all_resp_SPCN$bini)
+df_blocked_all_resp_SPCN_occip = filter(df_blocked_all_resp_SPCN, chindex == 12)
+df_blocked_all_resp_SPCN_PO7_PO8 = filter(df_blocked_all_resp_SPCN, chindex == 9)
+
+df_mixed_all_resp_N2pc$bini = factor(df_mixed_all_resp_N2pc$bini)
+df_mixed_all_resp_N2pc_occip = filter(df_mixed_all_resp_N2pc, chindex == 12)
+df_mixed_all_resp_N2pc_PO7_PO8 = filter(df_mixed_all_resp_N2pc, chindex == 9)
+
+df_mixed_all_resp_SPCN$bini = factor(df_mixed_all_resp_SPCN$bini)
+df_mixed_all_resp_SPCN_occip = filter(df_mixed_all_resp_SPCN, chindex == 12)
+df_mixed_all_resp_SPCN_PO7_PO8 = filter(df_mixed_all_resp_SPCN, chindex == 9)
+
+
+# electrode_site = 'occip' # 'occip' or 'PO7_PO8'
+electrode_site = 'PO7_PO8'
+condition = 'mixed'
+# condition = 'blocked'
+# response = 'correct'
+response = 'all_resp'
+time_window = 'SPCN'
+# time_window = 'N2pc'
+
+data_frame = paste('df',condition,response,time_window,electrode_site, sep = "_")
+data_frame
+
+current_anova = ezANOVA(
+  data = eval(parse(text = data_frame))
   , dv = .(value)
   , wid = .(ERPset)
   , within = .(bini)
 )
 
-print(mixed_correct_SPCN_PO7_PO8_anova)
+print(current_anova)
 
-mixed_correct_SPCN_PO7_PO8_anova_data_output = ezStats(
-  data = df_mixed_correct_SPCN_PO7_PO8
+current_anova_data_output = ezStats(
+  data = eval(parse(text = data_frame))
   , dv = .(value)
   , wid = .(ERPset)
   , within = .(bini)
 )
 
-print(mixed_correct_SPCN_PO7_PO8_anova_data_output)
+print(current_anova_data_output)
 
 ezPlot(
-  data = df_mixed_correct_SPCN_PO7_PO8
+  data = eval(parse(text = data_frame))
   , dv = .(value)
   , wid = .(ERPset)
   , within = .(bini)
   , x = bini
 )
-
-
-###### all responses:
-
-
 
 
